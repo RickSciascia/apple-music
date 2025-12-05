72b51eed4bfb1f60f4eb3cb4e37e9f46de56f085
Riccardo <richi-97@live.it>
2025-12-05 16:50:13 +0100

    final topbar

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,4 +1,4 @@
-import { Button, Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Row, Form } from "react-bootstrap";
 import StaticRadioCard from "./StaticRadioCard";
 
 const HeroSection = function () {
@@ -6,8 +6,8 @@ const HeroSection = function () {
     <Container fluid>
       <Row className="d-sm-none d-md-block">
         <Col xs={12} className="my-3">
-          <Row className="d-flex">
-            <Col xs={4} className="justify-content-center">
+          <Row className="d-flex justify-content-center align-content-center">
+            <Col xs={4} className="justify-content-center align-content-center">
               <div className="text-center">
                 <a className="text-white text-decoration-none">
                   <i className="bi bi-shuffle p-3"></i>
@@ -26,17 +26,20 @@ const HeroSection = function () {
                 </a>
               </div>
             </Col>
-            <Col xs={4}>
+            <Col xs={4} className="justify-content-center align-content-center">
               <div className="text-center">
                 <img src="../../public/apple.svg"></img>
               </div>
             </Col>
-            <Col xs={4}>
-              <div></div>
-              <Button variant="danger">
-                <i className="bi bi-person-fill pe-2"></i>
-                Accedi
-              </Button>
+            <Col xs={4} className="justify-content-center align-content-center">
+              <div className="d-flex align-items-center">
+                <i className="bi bi-volume-up-fill pe-2 text-white"></i>
+                <Form.Range className="w-50 flex-grow-1" />
+                <Button variant="danger ms-1">
+                  <i className="bi bi-person-fill pe-2"></i>
+                  Accedi
+                </Button>
+              </div>
             </Col>
           </Row>
         </Col>
