0606f6602b72b350d31ad06771861b0b4e4dbaa7
Riccardo <richi-97@live.it>
2025-12-05 10:24:09 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import MyNavbar from "./components/MyNavbar";
+
+function App() {
+  return (
+    <>
+      <MyNavbar />
+    </>
+  );
+}
+
+export default App;
